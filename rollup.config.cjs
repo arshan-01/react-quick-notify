a6a64a98f6517dfd4b70cba61bff3ffc744ee8c9
Arshan Nawaz <arshannawaz01@gmail.com>
2025-09-19 05:42:53 +0500

    first commit

---
+++ rollup.config.cjs
@@ -0,0 +1,41 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const { dts } = require('rollup-plugin-dts');
+
+const packageJson = require('./package.json');
+
+module.exports = [
+  {
+    input: 'src/lib/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.lib.json',
+        exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'],
+      }),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'dist/esm/types/lib/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ToastProvider } from './contexts/ToastContext';
+import ToastContainer from './components/ToastContainer';
+import ToastDemo from './components/ToastDemo';
+
+function App() {
+  return (
+    <ToastProvider>
+      <div className="min-h-screen">
+        <ToastDemo />
+        <ToastContainer />
+      </div>
+    </ToastProvider>
+  );
+}
+
+export default App;
